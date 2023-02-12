4034027b6c61503321aedbaff5ee639f414bd7f8
gyrOS <115223694+An0n151@users.noreply.github.com>
2023-02-13 00:27:19 +0200

    add xbox configuration scripts

---
+++ GToolbox/3._Configure_System/Xbox/Enable_Xbox_Services.bat
@@ -0,0 +1,18 @@
+:: ### gyrOS ###
+
+:: Enable WiFi
+
+@echo off
+setlocal EnableDelayedExpansion
+
+set "params=%*"
+cd /d "%~dp0" && ( if exist "%temp%\getadmin.vbs" del "%temp%\getadmin.vbs" ) && fsutil dirty query %systemdrive% 1>nul 2>nul || (  echo Set UAC = CreateObject^("Shell.Application"^) : UAC.ShellExecute "cmd.exe", "/k cd ""%~sdp0"" && %~s0 %params%", "", "runas", 1 >> "%temp%\getadmin.vbs" && "%temp%\getadmin.vbs" && exit /B )
+
+sc config XboxGipSvc start= demand
+sc config XblAuthManager start= demand
+sc config XblGameSave start= demand
+sc config XboxNetApiSvc start= demand
+
+echo Xbox services have been enabled.
+
+pause >nul
