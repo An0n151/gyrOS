416f12eb466efadba21aaf5ad3faf426fbc794b8
gyrOS <115223694+An0n151@users.noreply.github.com>
2023-02-28 10:48:44 +0200

    fix wifi enablement code

--- GToolbox/3._Configure_System/Wi-Fi/WiFi_Enable.bat
+++ GToolbox/3._Configure_System/Wi-Fi/WiFi_Enable.bat
@@ -8,6 +8,8 @@ setlocal EnableDelayedExpansion
 set "params=%*"
 cd /d "%~dp0" && ( if exist "%temp%\getadmin.vbs" del "%temp%\getadmin.vbs" ) && fsutil dirty query %systemdrive% 1>nul 2>nul || (  echo Set UAC = CreateObject^("Shell.Application"^) : UAC.ShellExecute "cmd.exe", "/k cd ""%~sdp0"" && %~s0 %params%", "", "runas", 1 >> "%temp%\getadmin.vbs" && "%temp%\getadmin.vbs" && exit /B )
 
+set "currentuser=%WinDir%\gyrOS\NSudo\NSudoLG.exe -U:C -P:E -Wait"
+
 sc config vwififlt start= system
 sc start vwififlt
 sc config WlanSvc start= auto
