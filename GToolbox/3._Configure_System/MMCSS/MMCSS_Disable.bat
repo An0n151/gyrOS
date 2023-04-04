a0f54afab4cd8c2f2ac5ade30fe1a83cadf90732
gyrOS <115223694+An0n151@users.noreply.github.com>
2023-04-04 15:52:37 +0300

    add multimedia class scheduler configuration

---
+++ GToolbox/3._Configure_System/MMCSS/MMCSS_Disable.bat
@@ -0,0 +1,17 @@
+:: gyrOS ::
+
+:: Disable MMCSS
+
+@echo off
+
+set "params=%*"
+cd /d "%~dp0" && ( if exist "%temp%\getadmin.vbs" del "%temp%\getadmin.vbs" ) && fsutil dirty query %systemdrive% 1>nul 2>nul || (  echo Set UAC = CreateObject^("Shell.Application"^) : UAC.ShellExecute "cmd.exe", "/k cd ""%~sdp0"" && %~s0 %params%", "", "runas", 1 >> "%temp%\getadmin.vbs" && "%temp%\getadmin.vbs" && exit /B )
+
+reg add "HKLM\SYSTEM\CurrentControlSet\Services\MMCSS" /v "Start" /t REG_DWORD /d "4" /f
+sc stop MMCSS
+
+echo.
+echo Success.
+echo.
+
+pause >nul
