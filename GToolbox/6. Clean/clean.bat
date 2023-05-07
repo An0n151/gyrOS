b34523573bed8ca0ac59abec98a308a59f89bea4
adiboiii3 <129665359+adiboiii3@users.noreply.github.com>
2023-05-08 00:45:56 +0200

    Create clean.bat

---
+++ GToolbox/6. Clean/clean.bat
@@ -0,0 +1,20 @@
+@echo off
+del /s /f /q c:\windows\temp\*.*
+rd /s /q c:\windows\temp
+md c:\windows\temp
+del /s /f /q C:\WINDOWS\Prefetch
+del /s /f /q %temp%\*.*
+rd /s /q %temp%
+md %temp%
+deltree /y c:\windows\tempor~1
+deltree /y c:\windows\temp
+deltree /y c:\windows\tmp
+deltree /y c:\windows\ff*.tmp
+deltree /y c:\windows\history
+deltree /y c:\windows\cookies
+deltree /y c:\windows\recent
+places
+deltree /y c:\windows\spool
+\printers
+del c:\WIN386.SWP
+cls
