0fad29d59bb671e30505e93545ca40a19978e5d6
gyrOS <115223694+An0n151@users.noreply.github.com>
2023-05-10 16:17:57 +0300

    new: additional ntfs tweaks

--- source/gyrOS_1909.bat
+++ source/gyrOS_1909.bat
@@ -1537,6 +1537,8 @@ fsutil behavior set disablelastaccess 1 > nul 2> nul
 fsutil behavior set Bugcheckoncorrupt 0 > nul 2> nul
 fsutil behavior set disable8dot3 1 > nul 2> nul
 fsutil behavior set disablecompression 1 > nul 2> nul
+fsutil behavior set disabledeletenotify 0 > nul 2> nul
+fsutil behavior set encryptpagingfile 0 > nul 2> nul
 reg add "HKLM\SYSTEM\CurrentControlSet\Services\rdyboost" /v "Start" /t REG_DWORD /d "4" /f > nul 2> nul
 reg add "HKLM\SYSTEM\CurrentControlSet\Control\Session Manager" /v "ProtectionMode" /t REG_DWORD /d "0" /f > nul 2> nul
 reg add "HKLM\SYSTEM\CurrentControlSet\Control\Session Manager\Memory Management" /v "DisablePagingExecutive" /t REG_DWORD /d "1" /f > nul 2> nul
