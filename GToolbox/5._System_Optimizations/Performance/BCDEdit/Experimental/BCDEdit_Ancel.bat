86b82fe85aa64b226fe4f0e422e4f1ffe0cea4d9
gyrOS <115223694+An0n151@users.noreply.github.com>
2023-05-10 16:22:38 +0300

    update: experimental bcdedit set of tweaks

---
+++ GToolbox/5._System_Optimizations/Performance/BCDEdit/Experimental/BCDEdit_Ancel.bat
@@ -0,0 +1,32 @@
+:: gyrOS ::
+
+:: Experimental BCDEdit ; Credits to Ancel
+
+@echo off
+setlocal EnableDelayedExpansion
+
+set "params=%*"
+cd /d "%~dp0" && ( if exist "%temp%\getadmin.vbs" del "%temp%\getadmin.vbs" ) && fsutil dirty query %systemdrive% 1>nul 2>nul || (  echo Set UAC = CreateObject^("Shell.Application"^) : UAC.ShellExecute "cmd.exe", "/k cd ""%~sdp0"" && %~s0 %params%", "", "runas", 1 >> "%temp%\getadmin.vbs" && "%temp%\getadmin.vbs" && exit /B )
+
+bcdedit /set useplatformclock No
+bcdedit /set useplatformtick No
+bcdedit /set firstmegabytepolicy UseAll
+bcdedit /set avoidlowmemory 0x8000000
+bcdedit /set nolowmem Yes
+bcdedit /set allowedinmemorysettings 0x0
+bcdedit /set isolatedcontext No
+bcdedit /set MSI Default
+bcdedit /set pae ForceEnable
+bcdedit /set nx optout
+bcdedit /set highestmode Yes
+bcdedit /set forcefipscrypto No
+bcdedit /set noumex Yes
+bcdedit /set ems No
+bcdedit /set extendedinput Yes
+bcdedit /set debug No
+
+echo.
+echo Success. Restart your system.
+echo.
+
+pause >nul
