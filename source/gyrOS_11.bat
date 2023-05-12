58302d10acd0d4ae310a68eb5ef6bd1e31d551bb
gyrOS <115223694+An0n151@users.noreply.github.com>
2023-05-12 15:26:55 +0300

    rework: disable tasks section

--- source/gyrOS_11.bat
+++ source/gyrOS_11.bat
@@ -1351,50 +1351,58 @@ for %%i in (
 	schtasks /Change /TN "\Microsoft\Windows\Application Experience\%%i" /disable
 ) > nul 2> nul
 
+for %%i in (
+	Defrag\ScheduledDefrag
+	DiskDiagnostic\Microsoft-Windows-DiskDiagnosticDataCollector
+	MemoryDiagnostic\ProcessMemoryDiagnosticEvents
+	MemoryDiagnostic\RunFullMemoryDiagnostic
+	Shell\IndexerAutomaticMaintenance
+	WaaSMedic\PerformRemediation
+	DiskFootprint\Diagnostics
+	DiskFootprint\StorageSense
+	BrokerInfrastructure\BgTaskRegistrationMaintenanceTask
+	CloudExperienceHost\CreateObjectTask
+	Registry\RegIdleBackup
+	SoftwareProtectionPlatform\SvcRestartTaskNetwork
+	SoftwareProtectionPlatform\SvcRestartTaskLogon
+	StateRepository\MaintenanceTasks
+	UPnP\UPnPHostConfig
+	RetailDemo\CleanupOfflineContent
+	InstallService\ScanForUpdates
+	InstallService\ScanForUpdatesAsUser
+	InstallService\SmartRetry
+	Printing\EduPrintProv
+	Ras\MobilityManager
+	DiskCleanup\SilentCleanup
+	Diagnosis\Scheduled
+	Wininet\CacheTask
+	NetTrace\GatherNetworkInfo
+	TPM\Tpm-HASCertRetr
+	TPM\Tpm-Maintenance
+	Sysmain\ResPriStaticDbSync
+	ApplicationData\appuriverifierdaily
+	ApplicationData\appuriverifierinstall
+	ApplicationData\DsSvcCleanup
+	PushToInstall\LoginCheck
+	Shell\FamilySafetyMonitor
+	Shell\FamilySafetyRefresh
+) do (
+	schtasks /Change /TN "\Microsoft\Windows\%%i" /disable
+) > nul 2> nul
+
 schtasks /Change /Disable /TN "\Microsoft\Windows\Application Experience\Microsoft Compatibility Appraiser" > nul 2> nul
 schtasks /Change /Disable /TN "\Microsoft\Windows\UpdateOrchestrator\Refresh Settings" > nul 2> nul
 schtasks /Change /Disable /TN "\Microsoft\Windows\UpdateOrchestrator\Schedule Scan" > nul 2> nul
 schtasks /Change /Disable /TN "\Microsoft\Windows\UpdateOrchestrator\Schedule Scan Static Task" > nul 2> nul
-schtasks /Change /Disable /TN "\Microsoft\Windows\Defrag\ScheduledDefrag" > nul 2> nul
-schtasks /Change /Disable /TN "\Microsoft\Windows\DiskDiagnostic\Microsoft-Windows-DiskDiagnosticDataCollector" > nul 2> nul
-schtasks /Change /Disable /TN "\Microsoft\Windows\MemoryDiagnostic\ProcessMemoryDiagnosticEvents" > nul 2> nul
-schtasks /Change /Disable /TN "\Microsoft\Windows\MemoryDiagnostic\RunFullMemoryDiagnostic" > nul 2> nul
 schtasks /Change /Disable /TN "\Microsoft\Windows\Power Efficiency Diagnostics\AnalyzeSystem" > nul 2> nul
 schtasks /Change /Disable /TN "\Microsoft\Windows\Device Information\Device" > nul 2> nul
 schtasks /Change /Disable /TN "\Microsoft\Windows\Device Information\Device User" > nul 2> nul
-schtasks /Change /Disable /TN "\Microsoft\Windows\Shell\IndexerAutomaticMaintenance" > nul 2> nul
-schtasks /Change /Disable /TN "\Microsoft\Windows\WaaSMedic\PerformRemediation" > nul 2> nul
-schtasks /Change /Disable /TN "\Microsoft\Windows\DiskFootprint\Diagnostics" > nul 2> nul
-schtasks /Change /Disable /TN "\Microsoft\Windows\DiskFootprint\StorageSense" > nul 2> nul
-schtasks /Change /Disable /TN "\Microsoft\Windows\BrokerInfrastructure\BgTaskRegistrationMaintenanceTask" > nul 2> nul
-schtasks /Change /Disable /TN "\Microsoft\Windows\CloudExperienceHost\CreateObjectTask" > nul 2> nul
-schtasks /Change /Disable /TN "\Microsoft\Windows\Registry\RegIdleBackup" > nul 2> nul
 schtasks /Change /Disable /TN "\Microsoft\Windows\Windows Filtering Platform\BfeOnServiceStartTypeChange" > nul 2> nul
-schtasks /Change /Disable /TN "\Microsoft\Windows\SoftwareProtectionPlatform\SvcRestartTaskNetwork"  > nul 2> nul
-schtasks /Change /Disable /TN "\Microsoft\Windows\SoftwareProtectionPlatform\SvcRestartTaskLogon" > nul 2> nul
-schtasks /Change /Disable /TN "\Microsoft\Windows\StateRepository\MaintenanceTasks" > nul 2> nul
-schtasks /Change /Disable /TN "\Microsoft\Windows\UPnP\UPnPHostConfig" > nul 2> nul
-schtasks /Change /Disable /TN "\Microsoft\Windows\RetailDemo\CleanupOfflineContent" > nul 2> nul
-schtasks /Change /Disable /TN "\Microsoft\Windows\InstallService\ScanForUpdates" > nul 2> nul
-schtasks /Change /Disable /TN "\Microsoft\Windows\InstallService\ScanForUpdatesAsUser" > nul 2> nul
-schtasks /Change /Disable /TN "\Microsoft\Windows\InstallService\SmartRetry" > nul 2> nul
 schtasks /Change /Disable /TN "\Microsoft\Windows\International\Synchronize Language Settings" > nul 2> nul
-schtasks /Change /Disable /TN "\Microsoft\Windows\Printing\EduPrintProv" > nul 2> nul
-schtasks /Change /Disable /TN "\Microsoft\Windows\Ras\MobilityManager" > nul 2> nul
 schtasks /Change /Disable /TN "\Microsoft\Windows\Time Zone\SynchronizeTimeZone" > nul 2> nul
 schtasks /Change /Disable /TN "\Microsoft\Windows\Time Synchronization\SynchronizeTime" > nul 2> nul
 schtasks /Change /Disable /TN "\Microsoft\Windows\Time Synchronization\ForceSynchronizeTime" > nul 2> nul
-schtasks /Change /Disable /TN "\Microsoft\Windows\DiskCleanup\SilentCleanup" > nul 2> nul
-schtasks /Change /Disable /TN "\Microsoft\Windows\Diagnosis\Scheduled" > nul 2> nul
-schtasks /Change /Disable /TN "\Microsoft\Windows\Wininet\CacheTask" > nul 2> nul
 schtasks /Change /Disable /TN "\Microsoft\Windows\Mobile Broadband Accounts\MNO Metadata Parser" > nul 2> nul
-schtasks /Change /Disable /TN "\Microsoft\Windows\NetTrace\GatherNetworkInfo" > nul 2> nul
-schtasks /Change /Disable /TN "\Microsoft\Windows\TPM\Tpm-HASCertRetr" > nul 2> nul
-schtasks /Change /Disable /TN "\Microsoft\Windows\TPM\Tpm-Maintenance" > nul 2> nul
-schtasks /Change /Disable /TN "\Microsoft\Windows\Sysmain\ResPriStaticDbSync" > nul 2> nul
-schtasks /Change /Disable /TN "\Microsoft\Windows\ApplicationData\appuriverifierdaily" > nul 2> nul
-schtasks /Change /Disable /TN "\Microsoft\Windows\ApplicationData\appuriverifierinstall" > nul 2> nul
-schtasks /Change /Disable /TN "\Microsoft\Windows\ApplicationData\DsSvcCleanup" > nul 2> nul
 schtasks /Change /Disable /TN "\Microsoft\Windows\.NET Framework\.NET Framework NGEN v4.0.30319" > nul 2> nul
 schtasks /Change /Disable /TN "\Microsoft\Windows\.NET Framework\.NET Framework NGEN v4.0.30319 64" > nul 2> nul
 schtasks /Change /Disable /TN "\Microsoft\Windows\.NET Framework\.NET Framework NGEN v4.0.30319 64 Critical" > nul 2> nul
@@ -1404,9 +1412,6 @@ schtasks /Change /Disable /TN "\Microsoft\XblGameSave\XblGameSaveTaskLogon" > nu
 schtasks /Change /Disable /TN "\Microsoft\WindowsManagement\Provisioning\Cellular" > nul 2> nul
 schtasks /Change /Disable /TN "\Microsoft\Windows\TaskScheduler\Maintenance Configurator" > nul 2> nul
 schtasks /Change /Disable /TN "\Microsoft\Windows\TaskScheduler\Regular Maintenance" > nul 2> nul
-schtasks /Change /Disable /TN "\Microsoft\Windows\PushToInstall\LoginCheck" > nul 2> nul
-schtasks /Change /Disable /TN "\Microsoft\Windows\Shell\FamilySafetyMonitor" > nul 2> nul
-schtasks /Change /Disable /TN "\Microsoft\Windows\Shell\FamilySafetyRefresh" > nul 2> nul
 schtasks /Change /Disable /TN "\Microsoft\Windows\Windows Media Sharing\UpdateLibrary" > nul 2> nul
 
 timeout /t 2 >nul
