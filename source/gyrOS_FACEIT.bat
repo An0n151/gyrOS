d0587b0e6fc64ac681a881a40dfec692d87c85d4
gyrOS <115223694+An0n151@users.noreply.github.com>
2023-05-13 11:08:54 +0300

    fix the for loop to disable sleep study

--- source/gyrOS_FACEIT.bat
+++ source/gyrOS_FACEIT.bat
@@ -1103,7 +1103,9 @@ reg add "HKLM\SOFTWARE\Microsoft\RemovalTools\MpGears" /v "SpyNetReportingLocati
 %currentuser% reg add "HKLM\SOFTWARE\Policies\Microsoft\AppV\CEIP" /v "CEIPEnable" /t REG_DWORD /d "0" /f > nul 2> nul
 
 :: Disable Sleep Study
-for %%a in ("SleepStudy" "Kernel-Processor-Power" "UserModePowerService") do (wevtutil sl Microsoft-Windows-%%a/Diagnostic /e:false)
+for %%a in ("SleepStudy" "Kernel-Processor-Power" "UserModePowerService") do (
+	wevtutil set-log Microsoft-Windows-%%a/Diagnostic /e:false
+)
 reg add "HKLM\SYSTEM\CurrentControlSet\Control\Power" /v "SleepStudyEnabled" /t REG_DWORD /d "0" /f > nul 2> nul
 reg add "HKLM\SYSTEM\CurrentControlSet\Control\Session Manager\Power" /v "SleepStudyDisabled" /t REG_DWORD /d "1" /f > nul 2> nul
 reg add "HKLM\SYSTEM\CurrentControlSet\Control\Power\PowerSettings\238C9FA8-0AAD-41ED-83F4-97BE242C8F20\9A6B7878-AD52-4AEC-9B44-767F1A8F3FDC" /v "Attributes" /t REG_DWORD /d "2" /f > nul 2> nul
