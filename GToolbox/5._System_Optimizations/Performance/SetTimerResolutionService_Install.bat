5ab2eccfc9abc9e0ecfc875e2cf882970c059463
Broma <81025171+daBroma@users.noreply.github.com>
2023-05-13 14:21:10 +0300

    TimerRes
    Change TimerRes script to use the https://github.com/amitxv/TimerResolution app instead

--- GToolbox/5._System_Optimizations/Performance/SetTimerResolutionService_Install.bat
+++ GToolbox/5._System_Optimizations/Performance/SetTimerResolutionService_Install.bat
@@ -7,9 +7,8 @@
 set "params=%*"
 cd /d "%~dp0" && ( if exist "%temp%\getadmin.vbs" del "%temp%\getadmin.vbs" ) && fsutil dirty query %systemdrive% 1>nul 2>nul || (  echo Set UAC = CreateObject^("Shell.Application"^) : UAC.ShellExecute "cmd.exe", "/k cd ""%~sdp0"" && %~s0 %params%", "", "runas", 1 >> "%temp%\getadmin.vbs" && "%temp%\getadmin.vbs" && exit /B )
 
-%WinDir%\gyrOS\SetTimerResolutionService.exe -install
-sc config STR start=auto
-net start STR
+cd "%AppData%\Microsoft\Windows\Start Menu\Programs\Startup"
+mklink "Set Timer Resolution.lnk" "%WinDir%\gyrOS\SetTimerResolution.exe" --no-console --resolution 5000
 
 echo.
 echo Service installed and started. Press any key to exit.
