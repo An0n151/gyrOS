c1193d0c5d285e9d2e200b498bb197fb5dc36e93
gyrOS <115223694+An0n151@users.noreply.github.com>
2023-05-13 19:37:03 +0300

    new: apex legends tweaks

---
+++ GToolbox/5._System_Optimizations/Games/Apex_Legends.bat
@@ -0,0 +1,17 @@
+:: gyrOS ::
+
+:: Apex Legends Tweaks
+
+@echo off
+
+set "params=%*"
+cd /d "%~dp0" && ( if exist "%temp%\getadmin.vbs" del "%temp%\getadmin.vbs" ) && fsutil dirty query %systemdrive% 1>nul 2>nul || (  echo Set UAC = CreateObject^("Shell.Application"^) : UAC.ShellExecute "cmd.exe", "/k cd ""%~sdp0"" && %~s0 %params%", "", "runas", 1 >> "%temp%\getadmin.vbs" && "%temp%\getadmin.vbs" && exit /B )
+
+reg add "HKLM\SYSTEM\CurrentControlSet\Control\PriorityControl" /v "ConvertibleSlateMode" /t REG_DWORD /d "0" /f
+reg add "HKLM\SYSTEM\CurrentControlSet\Control\PriorityControl" /v "IRQ8Priority" /t REG_DWORD /d "1" /f
+
+echo.
+echo Success.
+echo.
+
+pause >nul
