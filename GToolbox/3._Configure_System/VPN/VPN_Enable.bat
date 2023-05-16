b7312e61d4b052738cbb7fb97036a45853b41f1f
gyrOS <115223694+An0n151@users.noreply.github.com>
2023-05-16 22:59:07 +0300

    simplify devmanview code & add more devices

--- GToolbox/3._Configure_System/VPN/VPN_Enable.bat
+++ GToolbox/3._Configure_System/VPN/VPN_Enable.bat
@@ -7,12 +7,20 @@
 set "params=%*"
 cd /d "%~dp0" && ( if exist "%temp%\getadmin.vbs" del "%temp%\getadmin.vbs" ) && fsutil dirty query %systemdrive% 1>nul 2>nul || (  echo Set UAC = CreateObject^("Shell.Application"^) : UAC.ShellExecute "cmd.exe", "/k cd ""%~sdp0"" && %~s0 %params%", "", "runas", 1 >> "%temp%\getadmin.vbs" && "%temp%\getadmin.vbs" && exit /B )
 
-%WinDir%\gyrOS\DevManView.exe /enable "WAN Miniport (IPv6)"
-%WinDir%\gyrOS\DevManView.exe /enable "WAN Miniport (IKEv2)"
-%WinDir%\gyrOS\DevManView.exe /enable "WAN Miniport (L2TP)"
-%WinDir%\gyrOS\DevManView.exe /enable "WAN Miniport (IP)"
-%WinDir%\gyrOS\DevManView.exe /enable "Microsoft RRAS Root Enumerator"
-%WinDir%\gyrOS\DevManView.exe /enable "NDIS Virtual Network Adapter Enumerator"
+for %%i in (
+	"WAN Miniport (Network Monitor)"
+	"WAN Miniport (PPPOE)"
+	"WAN Miniport (PPTP)"
+	"WAN Miniport (SSTP)"
+	"WAN Miniport (IPv6)"
+	"WAN Miniport (IKEv2)"
+	"WAN Miniport (L2TP)"
+	"WAN Miniport (IP)"
+	"Microsoft RRAS Root Enumerator"
+	"NDIS Virtual Network Adapter Enumerator"
+) do (
+	start "" "%WinDir%\gyrOS\DevManView.exe" /enable %%i
+)
 
 reg add "HKLM\SYSTEM\CurrentControlSet\Services\SstpSvc" /v "Start" /t REG_DWORD /d "3" /f
 reg add "HKLM\SYSTEM\CurrentControlSet\Services\NdisVirtualBus" /v "Start" /t REG_DWORD /d "3" /f
