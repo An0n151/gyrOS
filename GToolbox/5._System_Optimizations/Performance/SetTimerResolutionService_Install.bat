1a8afa7545da9855f0361459f1007133a5627ba2
gyrOS <115223694+An0n151@users.noreply.github.com>
2023-05-16 09:44:25 +0300

    fix: command syntax error

--- GToolbox/5._System_Optimizations/Performance/SetTimerResolutionService_Install.bat
+++ GToolbox/5._System_Optimizations/Performance/SetTimerResolutionService_Install.bat
@@ -7,8 +7,7 @@
 set "params=%*"
 cd /d "%~dp0" && ( if exist "%temp%\getadmin.vbs" del "%temp%\getadmin.vbs" ) && fsutil dirty query %systemdrive% 1>nul 2>nul || (  echo Set UAC = CreateObject^("Shell.Application"^) : UAC.ShellExecute "cmd.exe", "/k cd ""%~sdp0"" && %~s0 %params%", "", "runas", 1 >> "%temp%\getadmin.vbs" && "%temp%\getadmin.vbs" && exit /B )
 
-cd "%AppData%\Microsoft\Windows\Start Menu\Programs\Startup"
-mklink "Set Timer Resolution.lnk" "%WinDir%\gyrOS\SetTimerResolutionService.exe" --no-console --resolution 5000
+mklink "%AppData%\Microsoft\Windows\Start Menu\Programs\Startup\Set_Timer_Resolution.lnk" "%WinDir%\gyrOS\SetTimerResolutionService.exe"
 
 echo.
 echo Success. Restart your system.
