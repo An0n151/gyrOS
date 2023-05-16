c463e286e060456e236e9a8b46ed81d57ad55f0d
gyrOS <115223694+An0n151@users.noreply.github.com>
2023-05-16 10:15:33 +0300

    fix: timerres script

--- GToolbox/5._System_Optimizations/Performance/SetTimerResolutionService_Install.bat
+++ GToolbox/5._System_Optimizations/Performance/SetTimerResolutionService_Install.bat
@@ -7,7 +7,16 @@
 set "params=%*"
 cd /d "%~dp0" && ( if exist "%temp%\getadmin.vbs" del "%temp%\getadmin.vbs" ) && fsutil dirty query %systemdrive% 1>nul 2>nul || (  echo Set UAC = CreateObject^("Shell.Application"^) : UAC.ShellExecute "cmd.exe", "/k cd ""%~sdp0"" && %~s0 %params%", "", "runas", 1 >> "%temp%\getadmin.vbs" && "%temp%\getadmin.vbs" && exit /B )
 
-mklink "%AppData%\Microsoft\Windows\Start Menu\Programs\Startup\Set_Timer_Resolution.lnk" "%WinDir%\gyrOS\SetTimerResolutionService.exe"
+set link="%AppData%\Microsoft\Windows\Start Menu\Programs\Startup\Set_Timer_Resolution.lnk"
+set target="%WinDir%\gyrOS\SetTimerResolutionService.exe"
+set args=--resolution 5000 --no-console
+
+:: Create the symbolic link
+mklink %link% %target%
+
+:: Create a shortcut to the batch file in the Startup folder
+echo @echo off > "%AppData%\Microsoft\Windows\Start Menu\Programs\Startup\Start_Timer_Resolution.bat"
+echo start "" /min %target% %args% >> "%AppData%\Microsoft\Windows\Start Menu\Programs\Startup\Start_Timer_Resolution.bat"
 
 echo.
 echo Success. Restart your system.
