1bc346c2028cebd793f8cf4b0bd05b9b0199843f
gyrOS <115223694+An0n151@users.noreply.github.com>
2023-05-20 11:18:15 +0300

    update SDIO to latest version

---
+++ applications/Snappy Driver Installer/tools/SDI/langs/move_all.bat
@@ -0,0 +1,53 @@
+copy gui_ar.ini arabic.txt
+copy gui_hy.ini armenian.txt
+copy gui_az.ini azerbaijan.txt
+copy gui_be.ini belarusian.txt
+copy gui_pt_BR.ini brazilian.txt
+copy gui_cs.ini czech.txt
+copy gui_en_US.ini english.txt
+copy gui_fa.ini farsi.txt
+copy gui_fr.ini french.txt
+copy gui_de.ini german.txt
+copy gui_el.ini greek.txt
+copy gui_hu.ini hungarian.txt
+copy gui_it.ini italian.txt
+copy gui_ja.ini japanese.txt
+copy gui_lv.ini latvian.txt
+copy gui_pl.ini polish.txt
+copy gui_pt_PT.ini portuguese.txt
+copy gui_ro.ini romanian.txt
+copy gui_ru.ini russian.txt
+copy gui_es.ini spanish.txt
+copy gui_sv.ini swedish.txt
+copy gui_tr.ini turkish.txt
+copy gui_uk.ini ukrainian.txt
+copy gui_nl.ini dutch.txt
+copy gui_vi.ini vietnamese.txt
+copy gui_bg.ini bulgarian.txt
+copy gui_he.ini hebrew.txt
+copy gui_lt.ini lithuanian.txt
+copy gui_et.ini estonian.txt
+copy gui_no.ini norwegian.txt
+copy gui_ka.ini georgian.txt
+copy gui_da.ini danish.txt
+copy gui_ko.ini korean.txt
+copy gui_th.ini thai.txt
+copy gui_ca.ini catalan.txt
+copy gui_zh_TW.ini chinese_tw.txt
+copy gui_id.ini indonesian.txt
+copy gui_hr.ini croatian.txt
+
+rem following exist in transifex but currently not supported in SDI
+rem copy gui_bn.ini bengali.txt
+rem copy gui_bs.ini bosnian.txt
+rem copy gui_hi.ini hindi.txt
+rem copy gui_mk.ini macedonian.txt
+rem copy gui_si.ini sinhala.txt
+rem copy gui_sq.ini albanian.txt
+rem copy gui_ms.ini malay.txt
+rem copy gui_sk.ini slovak.txt
+rem copy gui_sr.ini serbian.txt
+rem copy gui_fi.ini finnish.txt
+rem copy gui_zh.ini chinese.txt
+rem copy gui_sl.ini slovenian.txt
+rem copy gui_en.ini english.txt
