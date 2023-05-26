084ce5ee9c9707f61c7a6be51105c077afd108cf
gyrOS <115223694+An0n151@users.noreply.github.com>
2023-05-26 11:43:01 +0300

    new: optimize msi center

---
+++ GToolbox/5._System_Optimizations/Applications/MSI_Center.bat
@@ -0,0 +1,26 @@
+:: gyrOS ::
+
+:: Disable MSI Center Services
+
+@echo off
+
+set "params=%*"
+cd /d "%~dp0" && ( if exist "%temp%\getadmin.vbs" del "%temp%\getadmin.vbs" ) && fsutil dirty query %systemdrive% 1>nul 2>nul || (  echo Set UAC = CreateObject^("Shell.Application"^) : UAC.ShellExecute "cmd.exe", "/k cd ""%~sdp0"" && %~s0 %params%", "", "runas", 1 >> "%temp%\getadmin.vbs" && "%temp%\getadmin.vbs" && exit /B )
+
+sc stop MSI_Center_Service
+sc stop MSI_VoiceControl_Service
+sc stop MSI_Case_Service
+sc stop Mystic_Light_Service
+sc stop LightKeeperService
+
+reg add "HKLM\SYSTEM\CurrentControlSet\Services\MSI_Center_Service" /v "Start" /t REG_DWORD /d "3" /f
+reg add "HKLM\SYSTEM\CurrentControlSet\Services\MSI_VoiceControl_Service" /v "Start" /t REG_DWORD /d "3" /f
+reg add "HKLM\SYSTEM\CurrentControlSet\Services\MSI_Case_Service" /v "Start" /t REG_DWORD /d "3" /f
+reg add "HKLM\SYSTEM\CurrentControlSet\Services\Mystic_Light_Service" /v "Start" /t REG_DWORD /d "3" /f
+reg add "HKLM\SYSTEM\CurrentControlSet\Services\LightKeeperService" /v "Start" /t REG_DWORD /d "3" /f
+
+echo.
+echo Success.
+echo.
+
+pause >nul
